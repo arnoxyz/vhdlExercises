1286690db224a0cae9a117110bc1d3396ea8913f
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-01-02 16:11:17 -0500

    add chapter3

---
+++ chapter3/2_FSMModeling/src/test_pkg.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+--define some constants that can be used in the design
+package test_pkg is
+	--constant declaration
+	constant SCREEN_WIDTH : integer := 720;
+	constant SCREEN_HEIGHT : integer := 480;
+	
+end package;
+
+
