7534ba850ff0712f215fb0d2c97590dab725729b
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-01-24 04:09:35 -0500

    rework/rename _Template

---
+++ chapter1/_Template/src/template.vhd
@@ -0,0 +1,18 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.math_pkg.all;
+
+use work.test_pkg.all;
+
+-- TODO: add entity (generic: constants, ports: inputs/outputs)
+entity design_name is
+end entity;
+
+-- TODO: add implementation of design
+architecture beh of design_name is 
+-- TODO: add signals, declare-components
+begin 
+	-- TODO: add design (process, concurrent-statements, init-components)
+end architecture;
---
+++ chapter1/_Template/src/template_pkg.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+--constants, components, 
+package test_pkg is
+	--constant declaration
+	constant NAME : integer := 720;
+end package;
+
+
